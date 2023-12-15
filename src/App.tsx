a7bf8784bc3b90ebf25b665a4408a92f0ff94d1e
vijay5599 <vbvijay84@gmail.com>
2023-12-15 14:33:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+import BasicExample from './Basic';
+// import './App.css'
+
+function App() {
+  return (
+    <div>
+      <BasicExample />
+    </div>
+  );
+}
+
+export default App;
